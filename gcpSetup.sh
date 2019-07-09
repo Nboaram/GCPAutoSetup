4f3433e263ecf0a1b36c687c3e5032bcbdaad9f8
nboaram <nboaram1@hotmail.co.uk>
2019-07-09 16:02:16 +0100

    creating project now

--- gcpSetup.sh
+++ gcpSetup.sh
@@ -1,5 +1,5 @@
 gcp auth login
-gcloud config set project ExampleProject-1234567890 --name ExampleProject --set-as-default
+gcloud projects create ExampleProject-1234567890 --name ExampleProject --set-as-default
 gcloud compute project-info add-metadata --metadata google-compute-default-region=europe-west2,google-default-zone=europe-west2-b
 gcloud compute instances create ExampleVM --machine-type f1-micro --tags http-server --image ubuntu-1804-lts
 gcloud compute ssh ExampleVM
