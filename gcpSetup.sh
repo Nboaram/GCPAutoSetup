3b9e2aae64c32236879e2958131b01f870dfa291
nboaram <nboaram1@hotmail.co.uk>
2019-07-10 10:42:41 +0100

    reworked, functional code

--- gcpSetup.sh
+++ gcpSetup.sh
@@ -1,8 +1,4 @@
-gcp auth login
-gcloud projects create ExampleProject-1234567890 --name ExampleProject --set-as-default
-gcloud compute project-info add-metadata --metadata google-compute-default-region=europe-west2,google-default-zone=europe-west2-b
-gcloud compute instances create ExampleVM --machine-type f1-micro --tags http-server --image ubuntu-1804-lts
-gcloud compute ssh ExampleVM
-git clone https://github.com/Nboaram/MeanAutoSetup.git
-cd /MeanAutoSetup
-./MeanSetup.sh
+gcloud config set project example-project-246215
+gcloud compute instances create examplevm --zone europe-west2-b --machine-type f1-micro --tags http-server --image-project ubuntu-os-cloud --image-family ubuntu-1804-lts
+sleep 2 && gcloud compute ssh --zone europe-west2-b examplevm --command "sudo git clone https://github.com/Nboaram/MeanAutoSetup.git"
+gcloud compute ssh --zone europe-west2-b examplevm --command "./MeanAutoSetup/MeanSetup.sh"
